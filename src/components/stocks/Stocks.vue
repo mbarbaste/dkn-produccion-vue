4fcd3ee2dd9078c5b938f32f5714bacbba0a1cd7
Martin <mbarbaste@gmail.com>
2017-09-01 15:37:53 -0300

    Se inicializó módulo stock

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="contenedor _alignCenter">
+    <h5>Stocks</h5>
+    <br>
+    <p>En desarrollo</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
