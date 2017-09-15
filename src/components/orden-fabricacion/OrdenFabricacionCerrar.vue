1d1faae095f985662c509e1eb993840e142eb784
Martin <mbarbaste@gmail.com>
2017-09-15 17:16:03 -0300

    Se agregó funcionalidad de cerrar formación en ofab

--- src/components/orden-fabricacion/OrdenFabricacionCerrar.vue
+++ src/components/orden-fabricacion/OrdenFabricacionCerrar.vue
@@ -4,11 +4,11 @@
     <br> -->
 
     <!-- The Modal -->
-    <div id="myModal1" class="modalbox-modal" :class="{ dBlock: showModal }">
+    <div id="cerrar" class="modalbox-modal" :class="{ dBlock: showModal }">
     <!-- Modal content -->
       <div class="modalbox-modal-content">
         <span class="-close" id="modalbox-close1" @click="cerrarModal">✖</span>
-        <p>Confirma cerrar Orden de Fabricación {{ id }} ?</p>
+        <p>Confirma cerrar Orden de Fabricación <strong>{{ id }}</strong> ?</p>
         <fieldset>
           <legend>Observaciones</legend>
           <br>
@@ -21,11 +21,30 @@
       </div>
     </div>
 
+    <div id="cerrarFormacion" class="modalbox-modal" :class="{ dBlock: showModalFormacion }">
+    <!-- Modal content -->
+      <div class="modalbox-modal-content">
+        <span class="-close" id="modalbox-close1" @click="cerrarModal">✖</span>
+        <p>Confirma cerrar <strong>Formación</strong> de la Orden de Fabricación <strong>{{ id }}</strong> ?</p>
+        <fieldset>
+          <legend>Observaciones</legend>
+          <br>
+          <textarea class="_width100" v-model="ofab.observaciones" placeholder="Ingrese aquí tus observaciones.." id="observaciones"></textarea>
+          <div _clear _floatRight>
+            <button class="_primary" @click="cerrarOrdenFormacion">Cerrar Formacion</button>
+          </div>
+        </fieldset>
+
+      </div>
+    </div>
+
 
     <div v-if="!cerrada" class="jumbo b-light">
-      <h4>Cerrar Orden de Fabricación {{ id }}</h4>
+      <h4>Cerrar Orden de Fabricación <strong>{{ id }}</strong></h4>
       <h5> {{ ofab.articulo }} </h5>
-      <button class="_active" @click="abrirModal">Cerrar Orden</button>
+      <button class="_active" @click="abrirFormacionModal" v-if="!formacionCerrada">Cerrar Formacion</button>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button class="_danger" @click="abrirModal">Cerrar Orden</button>
     </div>
 
     <div v-if="cerrada" class="jumbo _warning">
@@ -55,10 +74,13 @@ export default {
         bizcocho: 0,
         blanco: 0,
         fecha_fin: '',
+        formacion_cerrada: '',
         observaciones: ''
       },
       showModal: false,
-      cerrada: false
+      showModalFormacion: false,
+      cerrada: false,
+      formacionCerrada: false
     }
   },
   computed: {
@@ -77,6 +99,10 @@ export default {
     ...mapMutations([
       'setProcessing'
     ]),
+    abrirFormacionModal() {
+      console.log('cerrar formacion')
+      this.showModalFormacion = true
+    },
     getOrdenFabricacion(id) {
       this.setProcessing(true)
       this.ofab.fecha_fin = this.getHoy;
@@ -85,8 +111,13 @@ export default {
           if (respuesta.data.length > 0) {
             this.ofab = respuesta.data[0]
             //console.log(respuesta.data[0].fecha_fin.length)
+            console.log(this.ofab)
             if(respuesta.data[0].fecha_fin !== null) {
               this.cerrada = true;
+            }
+
+            if(respuesta.data[0].formacion_cerrada == '1') {
+              this.formacionCerrada = true;
             }
 
           } else {
@@ -101,6 +132,33 @@ export default {
     },
     cerrarModal() {
       this.showModal = false
+      this.showModalFormacion = false
+    },
+    abrirFormacionModal() {
+        this.showModalFormacion = true
+    },
+    cerrarOrdenFormacion() {
+      this.setProcessing(true)
+      console.log('cerrar solo formacion')
+
+      let payload
+
+      payload = { id: this.id}
+
+      this.$http.post(this.getUrl + 'cerrar_formacion', payload)
+        .then(respuesta => {
+          if (respuesta.data.status == 'ok') {
+
+            console.log('se ha cerrado formacion')
+            this.formacionCerrada = true
+            this.showModalFormacion = false
+
+          } else {
+            console.log(respuesta.data.msg)
+          }
+        })
+
+      this.cerrarModal()
     },
     cerrarOrden() {
       this.ofab.fecha_fin = this.getHoy
